32b66e919c555136894460d78acc6797e9bf4c52
Surwase Vijaya <surwasev0814@gmail.com>
2022-04-01 01:39:27 +0530

    First Commit example-added one method

--- Self Study/src/com/selfStudy/FirstOne/BasicToLearnCommit.java
+++ Self Study/src/com/selfStudy/FirstOne/BasicToLearnCommit.java
@@ -1,10 +1,20 @@
 package com.selfStudy.FirstOne;
 
+
+
 public class BasicToLearnCommit {
 
 	public static void main(String[] args) {
+		//first one original
 	    System.out.println("Start exploring git with ur project");
+        
+	    //trying my first commit
+	   int result= add(2, 3);
+	   System.out.println("Addition is"+result);
+	}
 
+	private static int add(int a,int b) {
+		return a+b;
 	}
 
 }
