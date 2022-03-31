1d1f9a0eff8498625519449ec36d1e63fa73d08c
Surwase Vijaya <surwasev0814@gmail.com>
2022-04-01 00:27:45 +0530

    Write a new custom exception for setPrice() and display the error

---
+++ Thread-demo/src/Book.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+
+
+enum STATUS{
+	NEW,DAMAGED,RETURN
+}
+public class Book {
+	private String title;
+	private float price;
+	private STATUS status;
+	
+	public Book() {
+		this.status=STATUS.NEW;
+	}
+	public STATUS getStatus() {
+		return status;
+	}
+	public void setStatus(STATUS status) {
+		this.status=status;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title= title;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) throws InvalidPriceException  {
+		if(price<=0) {
+			throw new InvalidPriceException("price>0");
+		}
+		this.title= title;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return "[title="+title+",price"+price+"]";
+	}
+}
