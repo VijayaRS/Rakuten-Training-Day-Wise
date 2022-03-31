bdad0cc369608b0483e50942a2ad78e3855e751b
Surwase Vijaya <surwasev0814@gmail.com>
2022-04-01 01:28:32 +0530

    Basic program to explore git

---
+++ Self Study/src/com/selfStudy/FirstOne/BasicToLearnCommit.java
@@ -0,0 +1,10 @@
+package com.selfStudy.FirstOne;
+
+public class BasicToLearnCommit {
+
+	public static void main(String[] args) {
+	    System.out.println("Start exploring git with ur project");
+
+	}
+
+}
