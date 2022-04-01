0c27c8adfa1f4cb001d7d82410aead946683f66c
Surwase Vijaya <surwasev0814@gmail.com>
2022-04-01 15:19:02 +0530

    Servlet, Creating dynamic web pages

---
+++ web1/WebContent/app.js
@@ -0,0 +1,9 @@
+function save(){
+	const element =document.getElementById('age');
+	console.log(element.value);
+	
+	const elements =document.getElementsByTagName('input');
+	console.log(elements);
+	//how to get a table in our project
+	
+}
