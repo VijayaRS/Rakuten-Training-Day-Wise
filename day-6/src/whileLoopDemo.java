25fcb036b2386c05dec2b5e9785c667936130dee
Surwase Vijaya <surwasev0814@gmail.com>
2022-04-04 12:10:59 +0530

    Pattern using while loop

---
+++ day-6/src/whileLoopDemo.java
@@ -0,0 +1,24 @@
+
+public class whileLoopDemo {
+	private static void patternUsingWhile() {
+		// TODO Auto-generated method stub
+		int row=5;
+		while(row>0) {
+			int column=0;
+			while(column<row) {
+			System.out.print("*");
+			column++;
+			}
+			row--;
+			System.out.println("\n");
+		}
+		
+	}
+	public static void main(String[] args) {
+		ForLoopDemo f=new ForLoopDemo();
+		//f.printRow(5);
+		patternUsingWhile();
+	}
+
+
+}
