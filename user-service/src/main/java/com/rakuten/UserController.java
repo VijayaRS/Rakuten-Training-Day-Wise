cb6aed03527b990258f1a3b4455edb5e23415bb7
Surwase Vijaya <surwasev0814@gmail.com>
2022-04-07 12:47:28 +0530

    sprinboot using hibernate

--- user-service/src/main/java/com/rakuten/UserController.java
+++ user-service/src/main/java/com/rakuten/UserController.java
@@ -5,20 +5,23 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 	@Autowired
-	UserService service;
+	UserService service;//only reference
 
 	@PostMapping
+	@ResponseStatus(code=HttpStatus.CREATED)
 	void saveUser(@RequestBody User user) {
 		System.out.println(user.getName());
 		System.out.println(user.getAge());
@@ -43,10 +46,7 @@ public class UserController {
 
 	@GetMapping("/age/{age}")
 	List<User> getUsersByAge(@PathVariable int age) {
-//		if(age<=0) {
-//			throw new 
-//			IllegalArgumentException("age cannot be negative="+age);
-//		}
+		
 
 		System.out.println("getuserbyageScalled");
 		System.out.println(age);
--- user-service/src/main/java/com/rakuten/UserService.java
+++ user-service/src/main/java/com/rakuten/UserService.java
@@ -4,28 +4,37 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-	List<User> users=new ArrayList<>();
+	@Autowired
+	UserRepository repository;
+	
+	
 	void save(User user) {
-		users.add(user);
+		repository.save(user);
+		
 		System.out.println("save method called");
 	}
 	public List<User> getAllUsers() {
 		
-		return users;
+		return repository.findAll();
 	}
 	public List<User> getUserByName(String name) {
 		// TODO Auto-generated method stub
-		List<User> filteredUsers=users.stream().filter((user)->user.getName().equals(name)).collect(Collectors.toList());
+		List<User> filteredUsers=getAllUsers().stream().filter((user)->user.getName().equals(name)).collect(Collectors.toList());
 		
 		return filteredUsers;
 	}
 	public List<User> getUserByAge(int age) {
-		// TODO Auto-generated method stub
-		List<User> filteredUsers=users.stream().filter((user)->user.getAge()==age).collect(Collectors.toList());
+		if(age<=0) {
+			throw new 
+			IllegalArgumentException("age cannot be negative="+age);
+		}
+		List<User> filteredUsers=getAllUsers().stream().filter((user)->user.getAge()==age).collect(Collectors.toList());
+	
 		return filteredUsers;
 	}
 
